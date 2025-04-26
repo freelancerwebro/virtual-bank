cf7b1dbbfce5a1c6d4d515371b556f317b42f1ef
Sorin Marian <marian.sorin@yahoo.com>
2025-04-26 12:07:07 +0300

    Introduce new exceptions

---
+++ src/Exceptions/InsufficientFundsException.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VirtualBank\Exceptions;
+
+class InsufficientFundsException extends \Exception {}
---
+++ src/Exceptions/InvalidDepositException.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VirtualBank\Exceptions;
+
+class InvalidDepositException extends \Exception {}
